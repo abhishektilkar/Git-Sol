ab8391259f44c2d7af45a28cac3ec5d1736fb1e1
abhishektilkar <tilkarabhishek2000@gmail.com>
2024-08-25 18:54:20 +0530

    fixed version

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ const nextConfig = {
     images: {
         remotePatterns: [
             {
-                hostname: 'lh3.googleusercontent.com'
+                hostname: 'avatars.githubusercontent.com'
             }
         ]
     }
--- src/auth.ts
+++ src/auth.ts
@@ -5,7 +5,6 @@ import Google from "next-auth/providers/google";
 import GitHub from "next-auth/providers/github";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  adapter: PrismaAdapter(prisma),
   providers: [Google, GitHub],
   secret: process.env.NEXTAUTH_SECRET,
 })
